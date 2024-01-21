2ddca8eeb56609867fb229751b6c206510afd35a
IlyinIO <ilyin88ok@gmail.com>
2024-01-21 20:53:57 +0800

    v.1.0.1 First Steps

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Привет, Мир!");
         int a = 1;
         int b = 2;
         int c = a + b;
